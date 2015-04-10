5d51055760ed216a6c067cbc859d40c2c768057e
fintalex <fintalex@mail.ru>
2015-04-10 15:33:36 +0300

    промежуточный результат с получением Dynamic curs

--- Controllers/exChangeRates.cs
+++ Controllers/exChangeRates.cs
@@ -14,13 +14,50 @@ namespace x_rater.Controllers
         public List<ExChangeRates> GetRates()
         {
             CBService.DailyInfoSoapClient cbservice = new CBService.DailyInfoSoapClient(); //.GetCursOnDate(new DateTime());
-            DataSet curDataSet = cbservice.GetCursOnDate(DateTime.Now.AddDays(1));
+			DataSet curDataSet = cbservice.GetCursOnDate(DateTime.Now.AddDays(1));
 
-            DataTable dt = curDataSet.Tables[0];
-            List<ExChangeRates> list = dt.DataTableToList<ExChangeRates>();
+			DataTable dt = curDataSet.Tables[0];
+			List<ExChangeRates> list = new List<ExChangeRates>(); // dt.DataTableToList<ExChangeRates>();
+
+			
+			var c = cbservice.GetCursDynamic(DateTime.Now.AddYears(-1), DateTime.Now, "R01235");
+
+			var r = cbservice.GetCursDynamic(DateTime.Now.AddYears(-1), DateTime.Now, "R01235");
+
+			var r2 = cbservice.GetCursDynamicXML(DateTime.Now.AddDays(-100), DateTime.Now, "R01235");
+
+			DataSet r3 = cbservice.GetCursDynamic(DateTime.Now.AddDays(-100), DateTime.Now, "R01235");
 
             return list;
         }
+
+		/// <summary>
+		/// Получаем список всех валют с которыми работает ЦБ
+		/// </summary>
+		/// <returns></returns>
+		public List<Valute> GetListValutes()
+		{
+			CBService.DailyInfoSoapClient cbservice = new CBService.DailyInfoSoapClient();
+
+			DataSet dsEnumValue = cbservice.EnumValutes(false);
+
+			DataTable dtValutes = dsEnumValue.Tables[0];
+
+			List<Valute> listValutes = dtValutes.DataTableToList<Valute>();
+
+			return listValutes;
+		}
+
+		public void GetDynamicOfCurs(DateTime timeFrom, DateTime timeTo, string valuteCode)
+		{
+			CBService.DailyInfoSoapClient cbservice = new CBService.DailyInfoSoapClient();
+
+			DataSet dsDynamicCurs = cbservice.GetCursDynamic(timeFrom, timeTo, valuteCode);
+
+			DataTable dtDynamicCurs = dsDynamicCurs.Tables[0];
+
+			List<Valute> listValutes = dtDynamicCurs.DataTableToList<Valute>();
+		}
     }
 
     
---
+++ Models/ValuteCurs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace x_rater.Models
+{
+	public class ValuteCurs
+	{
+		public DateTime CursDate;
+		public string Vcode;
+		public int Vnom;
+		public double Vcurs;
+	}
+}
